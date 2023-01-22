5f0c2ecdf4152f8e6be399005c0321831d4c1f22
raul-aon <aonofrieseiraul@gmail.com>
2023-01-22 16:16:59 +0200

    initial commit

---
+++ app/src/main/java/ro/raul_aon/meal_planner/MainActivity.java
@@ -0,0 +1,14 @@
+package ro.raul_aon.meal_planner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
